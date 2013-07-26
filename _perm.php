807913553198f517b17f90b988de84d0bef368c1
georgebombadil <george-github@bombadil.org>
2013-07-26 02:02:03 -0400

    Updated _perm.php with correct coloring for staff users and to pull ban/warn data from the reports table.

--- _perm.php
+++ _perm.php
@@ -32,17 +32,20 @@ $colors = array(
         "police" => "BLUE"
     );
 
-// Colorize the username based upon the color stored in the db, and strikethrough banned players
-// Output is: " class="COLOR" " or " class="COLOR" banned "
+// Colorize the username based on rank or the color stored in the db
+// Also, grey background warned players and strikethrough banned players
+// Output is: " class="COLOR" " or " class="COLOR banned "
 function userCSSColor ($userID) {
 	global $conn, $colors;
 	
+	$date = time();
+
 	// Check if userID supplied, return default value otherwise
 	if ($userID) {
 		// Determine whether $userID represents the numeric player_id or player_name
 		$playerID = "";
 		$playerColor = "";
-		$banned = "";
+		$reported = "";
 		
 		if (ctype_digit($userID)) {
 			$stmt = $conn->prepare("SELECT player_id FROM player WHERE player_id = ?");
@@ -69,30 +72,52 @@ function userCSSColor ($userID) {
 
 		// We found a match and have a valid player_id
 		if ($playerID) {
-		  $stmt = $conn->prepare("SELECT property_value FROM player_property WHERE ( property_key = 'color' OR property_key = 'banned' ) AND player_id = ?");
+			// Get player's color attribute
+		  $stmt = $conn->prepare("SELECT property_key, property_value FROM player_property WHERE ( property_key = 'senioradmin' OR property_key = 'admin' OR property_key = 'mentor' OR property_key = 'builder' OR property_key = 'donator' OR property_key = 'color' ) AND player_id = ?");
 			$stmt->execute(array($playerID));
-		
 			$playerInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			$stmt->closeCursor();
-			if ($stmt->rowCount() > 1) {
-				foreach($playerInfo as $pI) {
-					if ($pI['property_value'] == "true") {
-						$banned = "banned";
-					}
-					else {
-						$playerColor = $pI['property_value'];
-					}
+			
+			// Set player color according to their staff rank, if defined
+			foreach($playerInfo as $temp => $val) {
+				if ($playerInfo[$temp]['property_key'] == "senioradmin" and $playerInfo[$temp]['property_value'] == "true") {
+					$playerColor = "vampire";
 				}
-				
+				if ($playerInfo[$temp]['property_key'] == "admin" and $playerInfo[$temp]['property_value'] == "true" and $playerColor != "vampire") {
+					$playerColor = "admin";
+				}
+				if ($playerInfo[$temp]['property_key'] == "mentor" and $playerInfo[$temp]['property_value'] == "true" and $playerColor != "vampire" and $playerColor != "admin") {
+					$playerColor = "police";
+				}
+				if ($playerInfo[$temp]['property_key'] == "builder" and $playerInfo[$temp]['property_value'] == "true" and $playerColor != "vampire" and $playerColor != "admin" and $playerColor != "mentor") {
+					$playerColor = "helper";
+				}
+				if ($playerInfo[$temp]['property_key'] == "donator" and $playerInfo[$temp]['property_value'] == "true" and $playerColor != "vampire" and $playerColor != "admin" and $playerColor != "mentor" and $playerColor != "helper") {
+					$playerColor = "donator";
+				}
+				if ($playerInfo[$temp]['property_key'] == "color" and !$playerColor) {
+					$playerColor = $playerInfo[$temp]['property_value'];
+				}
 			}
-			else {
-				$playerColor = $playerInfo[0]['property_value'];
-			}
-
+			
 			if (!$playerColor) {
 				$playerColor = "No Color / Tourist";
 			}
-			return " class=\"" . $colors[$playerColor] . " $banned\" ";
+			
+			// Get warned/banned status
+			$stmt = $conn->prepare("SELECT report_action FROM player_report WHERE subject_id = ? AND report_validuntil >= ? AND report_action IN ('softwarn', 'hardwarn', 'ban') ORDER BY report_id DESC");
+			$stmt->execute(array($playerID,$date));
+			$playerReport = $stmt->fetchColumn();
+			$stmt->closeCursor();
+			
+			if ($playerReport == "ban") {
+				$reported = " banned";
+			}
+			else if ($playerReport == "softwarn" or $playerReport == "hardwarn") {
+				$reported = " warned";
+			}
+
+			return " class=\"" . $colors[$playerColor] . "$reported\" ";
 		}
 		else {
 			return " class=\"" . $colors['No Color / Tourist'] . "\" ";
@@ -103,3 +128,4 @@ function userCSSColor ($userID) {
 	}
 	
 }
+?>
