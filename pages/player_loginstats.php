52ef871fe864895f1576096a6a5a051a89179029
Emil Hernvall <emil@znaptag.com>
2013-08-14 22:06:53 +0200

    Added online players to stats graph

--- pages/player_loginstats.php
+++ pages/player_loginstats.php
@@ -3,7 +3,7 @@
 checkIfOnline();
 
 $sql  = "SELECT date(from_unixtime(login_timestamp)) d, count(login_id) c, "
-      . "count(distinct player_id) uc FROM player_login ";
+      . "count(distinct player_id) uc, max(login_onlineplayers) op FROM player_login ";
 $sql .= "WHERE login_timestamp > unix_timestamp() - 30*86400 AND login_action = 'login' ";
 $sql .= "GROUP BY d ORDER BY d";
 
@@ -13,9 +13,9 @@ $stmt->execute(array());
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $data = array();
-$data[] = array("Date", "Logins", "Unique Logins");
+$data[] = array("Date", "Logins", "Unique Logins", "Max Online Players");
 foreach ($result as $row) {
-    $data[] = array($row["d"], intval($row["c"]), intval($row["uc"]));
+    $data[] = array($row["d"], intval($row["c"]), intval($row["uc"]), intval($row["op"]));
 }
 
 echo json_encode($data);
