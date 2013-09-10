afffa04da98e497c26738549632b43764a9a0bb3
Emil Hernvall <emil@znaptag.com>
2013-09-10 15:47:00 +0200

    Front page stats

--- pages/index.php
+++ pages/index.php
@@ -16,19 +16,46 @@ $stmt->execute();
 
 $logins = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$sql  = "SELECT * FROM player "; 
-$sql .= "WHERE NOT player_rank IN ('unverified', 'tourist') ";
-$sql .= "ORDER BY player_created DESC LIMIT 10";
+$sql  = "SELECT student_id, student.player_name student_name, "
+      . "mentor_id, mentor.player_name mentor_name, "
+      . "mentorlog_completedtime FROM mentorlog ";
+$sql .= "INNER JOIN player student ON student.player_id = student_id ";
+$sql .= "INNER JOIN player mentor ON mentor.player_id = mentor_id ";
+$sql .= "WHERE mentorlog_status = 'completed' ";
+$sql .= "ORDER BY mentorlog_completedtime DESC LIMIT 10";
 
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 
 $settlers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$sql  = "SELECT date(from_unixtime(login_timestamp)) d, count(login_id) c, "
+      . "count(distinct player_id) uc, max(login_onlineplayers) op FROM player_login ";
+$sql .= "WHERE login_timestamp > unix_timestamp() - 30*86400 "
+      . "AND login_action = 'login' ";
+$sql .= "GROUP BY d ORDER BY d";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute(array());
+
+$stats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$resultCount = count($stats);
+$loginsCount = $stats[$resultCount-1]["c"];
+$uniqueCount = $stats[$resultCount-1]["uc"];
+$maxPlayers = $stats[$resultCount-1]["op"]+1;
+$loginsTrend = trend($stats, "c");
+$uniqueTrend = trend($stats, "uc");
+
 $context = array();
 $context["players"] = tregmine_online_players($tregmineApiKey);
 $context["logins"] = $logins;
 $context["settlers"] = $settlers;
+$context["loginsCount"] = $loginsCount;
+$context["uniqueCount"] = $uniqueCount;
+$context["maxPlayers"] = $maxPlayers;
+$context["loginsTrend"] = $loginsTrend[count($loginsTrend)-1];
+$context["uniqueTrend"] = $uniqueTrend[count($uniqueTrend)-1];
 
 $styles = array();
 $scripts = array("/js/start.js");
--- pages/player_loginstats.php
+++ pages/player_loginstats.php
@@ -1,28 +1,6 @@
 <?php
 
-// Simple least square regressions and linear interpolation
-function trend($points, $idx) {
-    $A = 0; $B = 0; $C = 0; $D = 0;
-    $n = count($points);
-    foreach ($points as $x => $row) {
-        $y = $row[$idx];
-
-        $A += $x * $x;
-        $B += $x;
-        $C += $x * $y;
-        $D += $y;
-    }
-
-    $m = ($A * $D - $B * $C) / ($n * $A - $B * $B);
-    $k = ($C - $m * $B) / $A;
-
-    $result = array();
-    for ($x = 0; $x < $n; $x++) {
-        $result[] = round($k * $x + $m);
-    }
-
-    return $result;
-}
+require_once '../include/functions.php';
 
 $sql  = "SELECT date(from_unixtime(login_timestamp)) d, count(login_id) c, "
       . "count(distinct player_id) uc, max(login_onlineplayers) op FROM player_login ";
