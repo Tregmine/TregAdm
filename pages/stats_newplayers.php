2952cbf86b6f20465c99cb0853a783a620af0931
Emil Hernvall <emil@znaptag.com>
2013-09-16 11:19:02 +0200

    Mentor stats, new player stats

---
+++ pages/stats_newplayers.php
@@ -0,0 +1,20 @@
+<?php
+
+$sql  = "SELECT year(player_created) year, "
+      . "month(player_created) month, "
+      . "count(if(player_rank = 'unverified' or player_rank = 'tourist', 1, null)) unverified, "
+      . "count(if(player_rank != 'unverified' and player_rank != 'tourist', 1, null)) settlers "
+      . "FROM player ";
+$sql .= "GROUP BY year, month HAVING year != 0";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+$players = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$context = array("players" => $players);
+
+$styles = array();
+$scripts = array();
+render('stats_newplayers.phtml', 'New players', $context, $styles, $scripts);
+
