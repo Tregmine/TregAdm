25cad3d9e208422153d8c52f78d98078a7203d37
James <jamez.s.97@gmail.com>
2014-02-21 15:16:41 +0000

    Sell-Delete

---
+++ pages/economy_deleteSell.php
@@ -0,0 +1,23 @@
+<?php
+
+checkIfOnline();
+checkRank("junior_admin", "senior_admin");
+
+if (!array_key_exists("id", $_GET)) {
+    exit;
+}
+if (!array_key_exists("data", $_GET)) {
+    exit;
+}
+
+$item_id = $_GET['id'];
+$item_data = $_GET['data'];
+
+$sql  = "UPDATE item ";
+$sql .= "SET sellable = 'no', item_value = '0' ";
+$sql .= "WHERE item_id = ? AND item_data = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute(array($item_id, $item_data));
+
+header('Location: /index.php/economy/');
