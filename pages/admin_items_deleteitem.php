9f3fbc0604dc5e751f4a5d433a1f701051e7276a
James <jamez.s.97@gmail.com>
2014-02-22 13:49:55 +0000

    ItemDB

---
+++ pages/admin_items_deleteitem.php
@@ -0,0 +1,22 @@
+<?php
+
+checkIfOnline();
+checkRank("senior_admin");
+
+if (!array_key_exists("id", $_GET)) {
+    exit;
+}
+if (!array_key_exists("data", $_GET)) {
+    exit;
+}
+
+$item_id = $_GET['id'];
+$item_data = $_GET['data'];
+
+$sql  = "DELETE FROM item ";
+$sql .= "WHERE item_id = ? AND item_data = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute(array($item_id, $item_data));
+
+header('Location: /index.php/admin/items');
