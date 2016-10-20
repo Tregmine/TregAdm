c9ed84d4ff88d47695305b6d3e0452e17447429d
Eric Rabil <ericrabil@tregmine.com>
2016-10-19 18:21:17 -0700

    Added bundle versions!

--- include/settings.php
+++ include/settings.php
@@ -6,5 +6,6 @@ $settings = array(
 		"logo" => 1,
 		"playerheads" => false,
 		//Set to true to have minimal database queries and page data
-		"minimalhomepage" => false
+		"minimalhomepage" => false,
+		"bundleVersion" => "4.5.1 Beta 2"
 );
